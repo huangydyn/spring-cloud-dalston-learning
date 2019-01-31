ce77863828562fc28b5f2990d14da17757c60c1f
huangyan <yxhuang@thoughtworks.com>
2019-01-31 14:49:19 +0800

    add redis limit

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 This is a spring cloud example project, and contains spring cloud componet:
 
 - Eureca
+- Ribbon
 - Zuul
+- Redis Limit
 - Feign
+- Turbine
 - Seluth
 - Zipkin
 - Hytrix
--- eureka-consumer/src/main/java/demo/Application.java
+++ eureka-consumer/src/main/java/demo/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableTurbine
 @EnableFeignClients
 @SpringCloudApplication
 public class Application {
